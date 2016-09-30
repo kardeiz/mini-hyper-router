a439fac69b97fecb7e769e4f994da10322f2a718
jacob <jacob@localhost.localdomain>
2016-09-30 14:02:32 -0400

    add copyright statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+// Much of the body of this macro is taken from
+// https://github.com/tomaka/rouille/blob/master/src/router.rs
+// The original copyright notice is included below
+
+// Copyright (c) 2016 The Rouille developers
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
+
 #[macro_export]
 macro_rules! router {
     ($request:expr, $(($method:pat) ($($pat:tt)+) => $value:block),*) => {
